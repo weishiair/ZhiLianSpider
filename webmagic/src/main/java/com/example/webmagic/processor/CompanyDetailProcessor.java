0b25a68afd3003f031c0de09109a781432e5e1d0
jianwenkai <email@1024465898@qq.com>
2023-11-06 09:57:02 +0800

    优化随机点击类，删除无用代码

--- webmagic/src/main/java/com/example/webmagic/processor/CompanyDetailProcessor.java
+++ webmagic/src/main/java/com/example/webmagic/processor/CompanyDetailProcessor.java
@@ -8,6 +8,7 @@ import com.example.webmagic.util.UserAgentUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -33,48 +34,38 @@ public class CompanyDetailProcessor implements PageProcessor {
     @Autowired
     private UrlUtil urlUtil;  // 注入 UrlUtil
 
-
     private Site site = Site.me().setRetryTimes(3).setSleepTime(1000).setUserAgent(UserAgentUtils.randomUserAgent());
-
     private Random random = new Random();  // 创建一个 Random 对象来生成随机数
 
     @Override
     public void process(Page page) {
-        // 使用 urlUtil 确保 URL 是 HTTPS
         String httpsUrl = urlUtil.ensureHttps(page.getUrl().toString());
-
-        // 检查URL是否以 "http://company.zhaopin.com" 开头
-        if (!page.getUrl().toString().startsWith("https://company.zhaopin.com")) {
-            return;  // 如果不是，直接返回，跳过后续的解析逻辑
+        if (!httpsUrl.startsWith("https://company.zhaopin.com")) {
+            return;  // 如果不是智联招聘公司页，直接返回
         }
 
         WebDriver driver = webDriverProvider.getWebDriver();
         try {
-            driver.get(httpsUrl);  // 使用处理过的 HTTPS URL
-            boolean isSliderPresent = isSliderPresent(driver);
-            if (isSliderPresent) {
-                sliderHandler.handleSlider(driver);  // 调用 SliderHandler 来处理滑块
+            driver.get(httpsUrl);
+            if (isSliderPresent(driver)) {
+                sliderHandler.handleSlider(driver);
             }
 
-            // 定义你想要等待出现的元素的选择器
             By companyNameSelector = By.cssSelector("div.base-info");
-
-
-            // 设置最大等待时间（例如，10秒）
             WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
-            // 等待直到元素出现
-            wait.until(ExpectedConditions.visibilityOfElementLocated(companyNameSelector));
+            try {
+                wait.until(ExpectedConditions.visibilityOfElementLocated(companyNameSelector));
+            } catch (TimeoutException e) {
+                System.out.println("等待元素超时，跳过页面: " + httpsUrl);
+                return; // 超时则直接跳过后续代码执行
+            }
 
-            int randomWaitTime = 1000 + random.nextInt(2000);  // 生成一个随机的等待时间，范围是1000到3000毫秒（1到3秒）
+            int randomWaitTime = 1000 + random.nextInt(2000);
             Thread.sleep(randomWaitTime);
 
-            // 从WebDriver实例获取页面源代码
-            System.out.println("Current URL: " + driver.getCurrentUrl());
-            System.out.println("Current Title: " + driver.getTitle());
             String pageSource = driver.getPageSource();
             Document document = Jsoup.parse(pageSource);
-
             // 提取公司详细信息
             String establishmentDateStr = document.select("li:has(label:contains(成立时间)) em").text();
             String registeredCapital = document.select("li:has(label:contains(注册资本)) em").text();
@@ -91,67 +82,21 @@ public class CompanyDetailProcessor implements PageProcessor {
             // 将提取的公司信息数据存储在Page对象中
             page.putField("companyIntroduce", companyIntroduce);
 
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt(); // 重新设置中断状态
         } catch (Exception e) {
-            e.printStackTrace();
+            System.err.println("处理页面时发生异常: " + httpsUrl);
+            e.printStackTrace(); // 打印其他异常的堆栈跟踪
         }
     }
 
-    /*@Override
-    public void process(Page page) {
-        // 检查URL是否以 "http://company.zhaopin.com" 开头
-        if (!page.getUrl().toString().startsWith("http://company.zhaopin.com")) {
-            return;  // 如果不是，直接返回，跳过后续的解析逻辑
-        }
-
-        WebDriver driver = webDriverProvider.getWebDriver();
-        try {
-            driver.get(page.getUrl().toString());  // 确保 WebDriver 导航到正确的页面
-            boolean isSliderPresent = isSliderPresent(driver);
-            if (isSliderPresent) {
-                sliderHandler.handleSlider(driver);  // 调用 SliderHandler 来处理滑块
-            }
-            int sleepTime = 3000 + random.nextInt(5000);  // 生成一个随机的等待时间，范围是2000到7000毫秒
-            Thread.sleep(sleepTime);  // 暂停执行
-
-            // 从WebDriver实例获取页面源代码
-            System.out.println("Current URL: " + driver.getCurrentUrl());
-            System.out.println("Current Title: " + driver.getTitle());
-            String pageSource = driver.getPageSource();
-            Document document = Jsoup.parse(pageSource);
-
-            // 提取公司详细信息
-            String establishmentDateStr = document.select("li:has(label:contains(成立时间)) em").text();
-            String registeredCapital = document.select("li:has(label:contains(注册资本)) em").text();
-            String legalRepresentative = document.select("li:has(label:contains(法人代表)) em").text();
-            String companyAddress = document.select("div.map-box address").text();
-            // 提取公司的信息
-            String companyIntroduce = document.select("div.company-show__content__description").text();
-
-            // 将提取的数据存储在Page对象中
-            page.putField("establishmentDateStr", establishmentDateStr);
-            page.putField("registeredCapital", registeredCapital);
-            page.putField("legalRepresentative", legalRepresentative);
-            page.putField("companyAddress", companyAddress);
-            // 将提取的公司信息数据存储在Page对象中
-            page.putField("companyIntroduce", companyIntroduce);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }*/
-    public boolean isSliderPresent(WebDriver driver) {
-        // 使用 findElements 方法查找元素
+    private boolean isSliderPresent(WebDriver driver) {
         List<WebElement> sliders = driver.findElements(By.id("nc_1_nocaptcha"));
-        // 检查返回的列表是否为空
         return !sliders.isEmpty();
     }
-
-
 
     @Override
     public Site getSite() {
         return site;
     }
-
-
 }
--- webmagic/src/main/java/com/example/webmagic/util/RandomClicker.java
+++ webmagic/src/main/java/com/example/webmagic/util/RandomClicker.java
@@ -3,14 +3,21 @@ package com.example.webmagic.util;
 import com.example.webmagic.config.WebDriverProvider;
 import com.example.webmagic.scheduler.CookieTestTask;
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class RandomClicker implements Runnable {
@@ -18,47 +25,93 @@ public class RandomClicker implements Runnable {
     @Autowired
     private WebDriverProvider webDriverProvider;
 
-    private boolean running = true;  // 控制循环的标志
+    private boolean running = true;
 
     @Override
     public void run() {
         Random random = new Random();
         while (running) {
             try {
-                // 计算随机点击的时间点（0到30分钟之间）
                 long randomTime = random.nextInt(30 * 60 * 1000);
                 System.out.println("随机点击将在 " + (randomTime / 1000) + " 秒后执行。");
-
-                // 等待随机时间
                 Thread.sleep(randomTime);
 
                 WebDriver driver = webDriverProvider.getWebDriver();
-                String listPageUrl = CookieTestTask.getListPageUrl();  // 从CookieTestTask类获取列表页面的URL
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+                String listPageUrl = CookieTestTask.getListPageUrl();
                 if (listPageUrl != null) {
-                    driver.get(listPageUrl);  // 在执行随机点击前返回列表页面
+                    driver.get(listPageUrl);
                 }
 
-                // 执行随机点击
-                List<WebElement> clickableElements = driver.findElements(By.xpath("//a"));
+                List<WebElement> clickableElements = findClickableElements(driver);
+
                 if (!clickableElements.isEmpty()) {
                     WebElement randomElement = clickableElements.get(random.nextInt(clickableElements.size()));
+                    scrollToElementWithJS(driver, randomElement); // 滚动到元素位置
+                    simulateMouseMovement(driver, randomElement); // 模拟鼠标移动到元素上
+                    waitBeforeClick(random);
+
                     try {
+                        wait.until(ExpectedConditions.elementToBeClickable(randomElement));
                         randomElement.click();
-                        System.out.println("随机点击开始执行");  // 打印随机点击执行的消息
-                    } catch (Exception e) {
-                        System.err.println("在点击期间出现错误: " + e.getMessage());
+                    } catch (ElementClickInterceptedException e) {
+                        clickWithJS(driver, randomElement); // 使用JS点击
                     }
+
+                    System.out.println("随机点击执行成功。");
                 } else {
-                    System.err.println("随机点击未发现可以点击的元素.");
+                    System.err.println("没有发现可以点击的元素。");
                 }
 
-                // 管理窗口
                 manageWindows(driver);
-
             } catch (InterruptedException e) {
                 System.err.println("随机点击中断: " + e.getMessage());
+                Thread.currentThread().interrupt();
+            } catch (Exception e) {
+                System.err.println("在执行随机点击过程中出现异常: " + e.getMessage());
             }
         }
+    }
+    private void simulateMouseMovement(WebDriver driver, WebElement element) {
+        Actions actions = new Actions(driver);
+        actions.moveToElement(element).perform();
+    }
+
+    private List<WebElement> findClickableElements(WebDriver driver) {
+        // 找到所有的可点击且可见的元素
+        List<WebElement> allElements = driver.findElements(By.xpath("//a | //button"));
+        List<WebElement> clickableAndVisibleElements = allElements.stream()
+                .filter(WebElement::isDisplayed)
+                .filter(e -> {
+                    try {
+                        return ExpectedConditions.elementToBeClickable(e).apply(driver) != null;
+                    } catch (Exception ex) {
+                        return false;
+                    }
+                }).collect(Collectors.toList());
+
+        // 找到特定的三个元素
+        List<WebElement> preferredElements = driver.findElements(By.xpath("//div[@class='joblist-box__item clearfix']/a | //div[@class='listsort']/ul/li/a | //div[@class='iteminfo__line1__compname']/span[@class='iteminfo__line1__compname__name']"));
+
+        // 增加特定元素的权重，例如重复添加5次，这样它们就有更大的机会被选中
+        for (int i = 0; i < 5; i++) {
+            clickableAndVisibleElements.addAll(preferredElements);
+        }
+
+        return clickableAndVisibleElements;
+    }
+
+
+
+
+    private void scrollToElementWithJS(WebDriver driver, WebElement element) {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    private void waitBeforeClick(Random random) throws InterruptedException {
+        int timeToWait = random.nextInt(2000) + 500; // 确保至少等待500毫秒
+        Thread.sleep(timeToWait);
     }
 
     public void stop() {
@@ -69,20 +122,23 @@ public class RandomClicker implements Runnable {
         Set<String> windowHandles = driver.getWindowHandles();
         String originalWindowHandle = driver.getWindowHandle();
 
-        if (windowHandles.size() > 1) {
+        if (windowHandles.size() > 3) {
             for (String windowHandle : windowHandles) {
                 if (!windowHandle.equals(originalWindowHandle)) {
                     driver.switchTo().window(windowHandle);
                     driver.close();
                 }
             }
-            // Switch back to the original window
             driver.switchTo().window(originalWindowHandle);
         }
 
-        String listPageUrl = CookieTestTask.getListPageUrl();  // 从CookieTestTask类获取列表页面的URL
+        String listPageUrl = CookieTestTask.getListPageUrl();
         if (listPageUrl != null) {
-            driver.get(listPageUrl);  // 返回列表页面
+            driver.get(listPageUrl);
         }
+    }
+
+    private void clickWithJS(WebDriver driver, WebElement element) {
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
     }
 }
